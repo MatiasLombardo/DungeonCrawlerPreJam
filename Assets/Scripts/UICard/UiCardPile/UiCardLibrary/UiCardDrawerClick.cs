3a74cf245d036848d4e27e120498e808fbf021b7
guacamayoz <guacamayo@outlook.com>
2023-04-06 00:54:44 -0300

    Ajustes a la pantalla de combate, solo para optimizar

---
+++ Assets/Scripts/UICard/UiCardPile/UiCardLibrary/UiCardDrawerClick.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Tools.UI.Card
+{
+    [RequireComponent(typeof(IMouseInput))]
+    public class UiCardDrawerClick : MonoBehaviour
+    {
+        UiPlayerHandUtils CardDrawer { get; set; }
+        IMouseInput Input { get; set; }
+
+        void Awake()
+        {
+            CardDrawer = transform.parent.GetComponentInChildren<UiPlayerHandUtils>();
+            Input = GetComponent<IMouseInput>();
+            Input.OnPointerClick += DrawCard;
+        }
+        [SerializeField] bool isPlayer;
+        void DrawCard(PointerEventData obj) 
+        {
+            if (isPlayer)
+            {
+                CardDrawer.DrawCard();
+            }
+        }
+    }
+
+    
+}
